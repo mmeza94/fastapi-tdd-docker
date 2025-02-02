e631dcc7da8e23af835956cfd189bc87f97abd01
Manuel Meza <alex.meza.ramos200594@gmail.com>
2025-02-01 22:48:07 -0600

    summaries reformatted

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -3,8 +3,11 @@ from typing import List
 from fastapi import APIRouter, BackgroundTasks, HTTPException, Path
 
 from app.api import crud
-from app.models.pydantic import (SummaryPayloadSchema, SummaryResponseSchema,
-                                 SummaryUpdatePayloadSchema)
+from app.models.pydantic import (
+    SummaryPayloadSchema,
+    SummaryResponseSchema,
+    SummaryUpdatePayloadSchema,
+)
 from app.models.tortoise import SummarySchema
 from app.summarizer import generate_summary
 
